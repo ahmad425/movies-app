15ec6ef88e59a6de2e2e86f6cd622e65021ed36e
Ahmad <mohammadahmad100k@gmail.com>
2021-11-22 18:03:26 +0500

    Add favoutite movies count

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -5,5 +5,24 @@ ActiveAdmin.register User do
   filter :username
   filter :created_at
 
-  actions :index
+  actions :index, :show, :destroy
+
+  index do 
+    column :id
+    column :username
+    column '# favourited movies' do |r|
+      r.favorites.count
+    end
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :username
+      row '# favourited movies' do |r|
+        r.favorites.count
+      end
+    end
+  end
 end
